36d07cc61e6c1e33fed2391ccf3f7b3a577ac2f5
Erik <31732187+eln17011@users.noreply.github.com>
2022-05-21 15:27:07 +0200

    added flapfield

---
+++ ARC_Game/Assets/Scripts/FlapField.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlapField : MonoBehaviour
+{
+
+    float TBD;
+    public float flapForce;
+    private HashSet<GameObject> _inrange = new HashSet<GameObject>();
+    Collider ff;
+
+    void OnDisable()
+    {
+        _inrange.Clear();
+    }
+
+    void OnTriggerEnter(Collider c) //change to 2d for 2d
+    {
+        if(c.gameObject.tag == "Player")
+        {
+            _inrange.Add(c.gameObject);
+            Debug.Log("Player entered my safe space");
+        }
+        
+    }
+
+    void OnTriggerExit(Collider c) //change to 2d for 2d
+    {
+        _inrange.Remove(c.gameObject);
+        Debug.Log("Player respects my safe space");
+    }
+
+
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach(GameObject gB in _inrange){
+            Rigidbody rig = gB.GetComponent<Rigidbody>();
+            new Vector3 dir = (100f, 100f, 100f);
+            rig.AddForce(flapForce * dir);
+        }
+    }
+}
