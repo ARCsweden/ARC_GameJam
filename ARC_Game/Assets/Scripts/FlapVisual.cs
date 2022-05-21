9e1bf9ff6c026ab8b23e5bdf6b808113ede3f647
felix sjöqvist <fst17001@student.mdh.se>
2022-05-21 13:35:17 +0200

    added non functional fin

---
+++ ARC_Game/Assets/Scripts/FlapVisual.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlapVisual : MonoBehaviour
+{
+    public float finPos;
+    public FlapInput flapper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
